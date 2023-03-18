128ccd3c28e52426869baa81bd263d27489f9fd1
pasca <pasca@Pascal.speedport.ip>
2023-03-18 11:31:21 +0100

    sd

--- HelloJavaWorld/src/HelloJavaWorld.java
+++ HelloJavaWorld/src/HelloJavaWorld.java
@@ -3,7 +3,7 @@ public class HelloJavaWorld {
 
 	public static void main(String[] args) {
 		
-		int dirk = 0;
+		int dirk = 3;
 		
 		while(dirk<5) {
 		System.out.println("Hello JavaWorld "+ dirk);
